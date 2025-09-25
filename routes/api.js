bf757a574311fca00f57087411a73345e428754b
Taylor Hughes <taylorhughess.tjh@gmail.com>
2025-09-24 17:18:49 -0700

    Reflecting the addition of Authentication control functionality

--- routes/api.js
+++ routes/api.js
@@ -3,18 +3,22 @@ import {getIndexPage, getLoginForm, createUserLogin,
     resetLogin, loginUserAccount, linkAttributesPage,
     pageUnderConstruction} from '../controllers/homeController.js';
 
+import {login} from '../controllers/authenticateController.js';
+
 
 const router = express.Router();
 
 router.get('/', getIndexPage);
 router.get('/login', getLoginForm);
+router.post('/user-login', login); 
+
 router.get('/create-user-login', createUserLogin);
 router.get('/forgot-login', resetLogin);
 router.get('/attributes', linkAttributesPage);
 router.get('/PUC', pageUnderConstruction)
 
 
-router.post('/user-login', loginUserAccount);
+// router.post('/user-login', loginUserAccount);
 
 
 export default router;
